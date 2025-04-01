35c118668a5c93b52c8c8669661d09e655733f66
arshavardhan17 <2300031000@kluniversity.in>
2025-04-01 07:58:12 +0530

    Add examples demonstrating async/await syntax and usage in JavaScript

---
+++ 0-1/week 2/week2.1/01-AsyncAwait.js
@@ -0,0 +1,31 @@
+//  Async/Await is a new way to write asynchronous code. It is built on top of promises, so it is also non-blocking.
+//  It makes the code look like it is synchronous, but it is asynchronous.
+//  We use the async keyword to define an async function, and the await keyword to wait for a promise.
+//  The await keyword can only be used inside an async function.
+//  An async function will always return a promise.
+//  If the function returns a value, the promise will be resolved with the value.
+//  If the function throws an exception, the promise will be rejected.
+
+// Example 1
+async function fetchData() {
+  //  console log a will not be executed until the fetch is completed
+  let a = await fetch("https://randomuser.me/api");
+  a = await a.json();
+  console.log(a);
+}
+
+fetchData();
+
+// Example 2
+
+function KiratsAsyncFunction() {
+  let p = new Promise((resolve, reject) => {
+    // do some async work
+    resolve("Hello from Kirat's async function!");
+  });
+  return p;
+}
+async function main() {
+  let result = await KiratsAsyncFunction();
+  console.log(result);
+}
