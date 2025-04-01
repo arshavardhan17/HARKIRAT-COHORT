d41f79e6130567d8e39a4449de01682fa25e2713
arshavardhan17 <2300031000@kluniversity.in>
2025-04-01 10:34:26 +0530

    Add examples demonstrating query parameters handling in Express.js and create a readme for week 3.0

--- 0-1/week 2/week2.2/03-QuaryParameters.js
+++ 0-1/week 2/week2.2/03-QuaryParameters.js
@@ -1,11 +1,35 @@
 //for express to get json body data we need to use body-parser middleware
 
+//Example: 1
+
+const express = require("express");
+const app1 = express();
+function sum(n) {
+  let ans = 0;
+  for (let i = 0; i <= n; i++) {
+    ans += i;
+  }
+  return ans;
+}
+app1.get("/", function (req, res) {
+  const n = req.query.n;
+  const ans = sum(n);
+  res.send("Hi There Sum is: " + ans);
+});
+
+app1.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// open browser - localhost:3000/?n=20  (after ? - there is query parameters)
+
+//Example: 2
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.post("/submit", (req, res) => {
-  console.log(req.query.message); // to access the data sent in the request body
+  console.log(req.query.message);
   res.send("Data received");
 });
 
